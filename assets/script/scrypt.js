3c4c1b3455cb89023391dd04b36ede86d864a681
KATYA SYLENE FONTENELLE PALHETA <109077065+KatyaPalheta@users.noreply.github.com>
2022-08-29 03:15:57 -0300

    update
    finalizaçao da versão mobile

--- assets/script/scrypt.js
+++ assets/script/scrypt.js
@@ -15,3 +15,21 @@ function teste() {
 		botao.disabled = false;
 	}
 }
+
+const form2 = document.getElementById('form_email2');
+const email2 = document.getElementById('email2');
+const span2 = document.getElementById('mensagem_email2');
+const botao2 = document.getElementById('send_email2');
+
+function teste2() {
+	const valida2 = email2.validity.valid;
+	console.log(valida2);
+	if (valida2 === false) {
+		email2.classList.add('erro_email');
+		span2.classList.add('mensagem_erro_email');
+	} else {
+		email2.classList.remove('erro_email');
+		span2.classList.remove('mensagem_erro_email');
+		botao2.disabled = false;
+	}
+}
